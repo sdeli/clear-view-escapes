5e62da8e9a4014ef973312831cb83540dc7d2025
sdeli <bgfkszmsdeli@gmail.com>
2018-03-29 22:08:29 +0100

    no message

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.export = {
+
+	entry: {
+		main: "./exercise-sites/travel-site/app/assets/js/app.js"
+	},
+	output: {
+		filename: "./exercise-sites/travel-site/app/assets/js/bundle.js"	
+	}
+
+}
