c653f90e3eeb3657dcdf5320d028b7fef6c5026f
sdeli <bgfkszmsdeli@gmail.com>
2018-04-01 15:45:56 +0100

    on scroll fade in finished

--- exercise-sites/travel-site/app/assets/js/app.js
+++ exercise-sites/travel-site/app/assets/js/app.js
@@ -1,7 +1,9 @@
+import $ from 'jquery';
 import MainMenu from './modules/_main-menu.js'
 import RevealOnScroll from './modules/_reveal-on-scroll.js'
 
 var mainMenu = new MainMenu();
-var revealOnScroll = new RevealOnScroll();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "85%");
 
 
--- exercise-sites/travel-site/app/assets/js/modules/_reveal-on-scroll.js
+++ exercise-sites/travel-site/app/assets/js/modules/_reveal-on-scroll.js
@@ -1,12 +1,36 @@
 import $ from 'jquery';
+import waypoints from '../../../../../../node_modules/waypoints/lib/noframework.waypoints.js';
 
 class RevealOnScroll {
 
-	constructor(){
-		this.itemsToReveal = $()
-
+	constructor(els, offset){
+		this.itemsToReveal = els;
+		this.offcetPercentage = offset;
+		this.hideInitially();
+		this.createWaypoints();
 	}
 
-}
+	hideInitially(){
+		this.itemsToReveal.addClass("reveal-item");
+	}
+
+	createWaypoints(){
+		var that = this
+		console.log(that.offcetPercentage);
+		console.log(this.itemsToReveal);
+		this.itemsToReveal.each(function(index, item){
+
+			new Waypoint({
+				element: item,
+				handler: function(){
+					$(item).addClass("reveal-item--is-visible");
+				},
+				offset: that.offcetPercentage
+			}) //Waypont
+
+		}) //each
+	} // createWayponts
+
+} // class RevealOnScroll
 
 export default RevealOnScroll;
